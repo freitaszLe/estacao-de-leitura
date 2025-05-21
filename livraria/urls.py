0ca91341c5c1afa5bb85fb427914c37e7cc19625
freitaszLe <leticia.freitasz712@gmail.com>
2025-05-21 17:55:00 -0400

    pagina comprar listando livros e generos

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,5 @@ urlpatterns = [
     path('sobre/', views.about, name='about'),
     path('book/', views.book, name='cadastro'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
